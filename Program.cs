711aea0712d1d7c04c9080c541539e0c480d97e8
opilane <opilane@HP-CZC0359NNZ>
2023-02-13 13:51:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new List<List<string>>
+            {
+                new List<string> {"tea","roar","car","sky"},
+                new List<string> {"cup","falcon","loan","airplane"},
+                new List<string> {"head","snow","frost","fish"},
+            };
+            foreach (var nested in words)
+            {
+                foreach (var word in nested)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
